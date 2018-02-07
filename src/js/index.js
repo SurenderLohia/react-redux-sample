4d6c96a04719cdf41588277cf13f4fc25487ba0b
Surender Lohia <surender.lohia.e@gmail.com>
2018-02-07 13:10:45 +0530

    Cool react-redux app has been completed.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
-console.log("I am a boilerplate :-)");
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '../js/store/index';
+import { addArticle } from '../js/actions/index';
+import App from './components/App';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
+
+window.store = store;
+window.addArticle = addArticle;
